42ba1eaf68c9eba3141c83dcca51eb5eb2f1f91d
ivansglazunov <ivansglazunov@gmail.com>
2015-02-11 16:44:55 +0000

    Travis fix.

--- test.js
+++ test.js
@@ -6,22 +6,22 @@ global.assert = require('chai').assert;
 global.is = require('is_js');
 
 describe('osws-templates@0.3.0', function() {
-    require('./sources/compiler/test.js');
+    require(__dirname + '/sources/compiler/test.js');
     
-    require('./sources/sync/test.js');
-    require('./sources/async/test.js');
+    require(__dirname + '/sources/sync/test.js');
+    require(__dirname + '/sources/async/test.js');
     
-    require('./sources/prototype/test.js');
-    require('./sources/data/test.js');
-    require('./sources/tag/test.js');
-    require('./sources/single/test.js');
-    require('./sources/double/test.js');
-    require('./sources/doctype/test.js');
-    require('./sources/xml/test.js');
+    require(__dirname + '/sources/prototype/test.js');
+    require(__dirname + '/sources/data/test.js');
+    require(__dirname + '/sources/tag/test.js');
+    require(__dirname + '/sources/single/test.js');
+    require(__dirname + '/sources/double/test.js');
+    require(__dirname + '/sources/doctype/test.js');
+    require(__dirname + '/sources/xml/test.js');
     
-    require('./sources/mixin/test.js');
+    require(__dirname + '/sources/mixin/test.js');
     
-    require('./sources/render/test.js');
+    require(__dirname + '/sources/render/test.js');
     
     it('context', function() {
         assert.equal(T.doubles.div()('<%= a %>').context({ a: 1 }), '<div>1</div>');
