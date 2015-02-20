98c1025e217f36012450649fe9295d967ddc2ca0
Ivan S Glazunov <ivansglazunov@gmail.com>
2015-02-20 01:17:54 +0000

    Fix test include. alpha.14.

--- sources/compiler/include0.js
+++ sources/compiler/include0.js
@@ -1,6 +1,8 @@
 // Check native require.
 var T = require('../../');
+var path = require('path');
+
 // Check Templates.include.
-if (T.include('include1.js') != __dirname + '/include1.js') throw new Error('Unexpected T.');
+if (T.include('include1.js') != path.join(__dirname, '/include1.js')) throw new Error(T.include('include1.js') + ' != ' + path.join(__dirname, '/include1.js'));
 // Export test string.
 module.exports = __filename;
