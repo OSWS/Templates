a0e4a55a3542974577204293648918f7a78077be
Ivan S Glazunov <ivansglazunov@gmail.com>
2015-02-21 11:21:29 +0000

    Fix factory.

--- oswst.js
+++ oswst.js
@@ -1,17 +1,18 @@
 (function(factory) {
     if(typeof define === 'function' && define.amd) {
         define(['module', 'lodash', 'async'], function(module, _, async) {
-            module.exports = factory({}, _, async);
+            module.exports = factory( _, async);
         });
     } else if(typeof exports === 'object') {
-        module.exports = factory({}, require('lodash'), require('async'));
+        module.exports = factory(require('lodash'), require('async'));
     }
     var Factory = function() {
-        return factory({}, Array.prototype.slice.call(arguments, 1));
+        return factory(arguments);
     };
     if (typeof(window) == 'object') window.oswst = Factory;
     if (typeof(global) == 'object') global.oswst = Factory;
-})(function(T, _, async) {
+})(function(_, async) {
+    var T = {};
 
 // Available only on the server!
 
